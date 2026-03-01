bf0701d02bc7365ffec7f3c293c43dd82db6ee3a
Funmilayo <fummyoshe@gmail.com>
2026-03-01 14:39:19 -0500

    Changes

---
+++ app.js
@@ -0,0 +1,9 @@
+var msg = "Hello World!";
+console.log(msg);
+alert(msg);
+
+let base = 10;
+let height = 5;
+
+let area = (base * height) / 2;
+console.log("The area of the triangle with base " + base + " and height " + height + " is " + area);
